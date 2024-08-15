bbfc406c2a12e432e374ab9eb6db71b4edce546f
Tyunat <020182tnm@gmail.com>
2024-08-15 19:18:33 +0300

    Initial commit

---
+++ src/test/java/BankCardTest.java
@@ -0,0 +1,46 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class BankCardTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void BeforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        //options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+
+    }
+
+    @Test
+    void CheckOrderBankCard() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Бурлакова Фрося");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+78888888888");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.xpath("//button[@role='button']")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
+
+    }
+
+}
+
+
